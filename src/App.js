92bf8e2cc86284ee77958298574ff91d22fe3fa2
user <ujjalroy7862@gmail.com>
2023-03-13 01:22:25 +0600

    here i know how react maninly work under the hood

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
-const users = [
-    {
-        fullName: 'Ujjal kumar Roy',
-        age: 23,
-        phones: [{ home: '191002326' }, { office: '191002325' }]
-    },
-    {
-        fullName: 'Haoitigo Roy',
-        age: 18,
-        phones: [{ home: '343233434' }, { office: '342345423' }]
-    }
-];
+function Todo() {
+    return React.createElement('div', {}, React.createElement('p', {}, 'Todo TItle'));
+}
+function Todo2() {
+    return (
+        <>
+            <div>
+                <p>Todo TItle 2</p>
+                <p>Todo TItle 3</p>
+            </div>
+        </>
+    );
+}
 const App = () => {
     return (
         <>
-            <h1 className="text-3xl font-bold text-center">React App</h1>
-            {users.map((el) => (
-                <div key={uuidv4()}>
-                    <h1>{el.fullName}</h1>
-                    <h1>{el.age}</h1>
-
-                    {el.phones.flat().map((el) => (
-                        <div key={uuidv4()}>
-                            <h1>
-                                {Object.keys(el)[0]}
-                                {Object.values(el)[0]}
-                            </h1>
-                        </div>
-                    ))}
-                </div>
-            ))}
+            <Todo />
+            <Todo2 />
         </>
     );
 };
