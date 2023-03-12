41fb75c5c3ac0edf7231b3005ea1168fc91ef2ff
user <ujjalroy7862@gmail.com>
2023-03-13 01:23:16 +0600

    remove some unsual file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React from 'react';
 
-function Todo() {
-    return React.createElement('div', {}, React.createElement('p', {}, 'Todo TItle'));
-}
-function Todo2() {
-    return (
-        <>
-            <div>
-                <p>Todo TItle 2</p>
-                <p>Todo TItle 3</p>
-            </div>
-        </>
-    );
-}
 const App = () => {
-    return (
-        <>
-            <Todo />
-            <Todo2 />
-        </>
-    );
+    return <></>;
 };
 
 export default App;
