1340f19584ee564f40358baa779afcf014f35127
user <ujjalroy7862@gmail.com>
2023-03-13 01:50:22 +0600

    learn very kopa things

--- src/components/State.js
+++ src/components/State.js
@@ -13,12 +13,20 @@ export default class State extends Component {
             count: this.state.count + 1
         });
     };
+    decrement = () => {
+        this.setState({
+            count: this.state.count - 1
+        });
+    };
     render() {
         return (
             <div>
                 <h1> CountValueis: {this.state.count}</h1>
-                <button className="p-2 m-5 bg-red-500 cursor-pointer" type="button" onClick={this.increment}>
+                <button className="p-2 m-5 bg-red-500 cursor-pointer" type="button" onClick={this.increment} disabled={this.state.count === 10 ? true : false}>
                     +
+                </button>
+                <button className="p-2 m-5 bg-red-500 cursor-pointer" type="button" onClick={this.decrement} disabled={this.state.count === 0 ? true : false}>
+                    -
                 </button>
             </div>
         );
