51fe8c6de243bc4434af1066cf467d5d440b7527
user <ujjalroy7862@gmail.com>
2023-03-13 13:43:30 +0600

    done to another example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import Child from './CHILD-TO_PARENT/Child';
+import Todo from './CHILD-TO_PARENT/Todo';
 
 const App = () => {
     const handleChildData = (childData) => {
         console.log(childData);
     };
+    const handleTodoData = (childData) => {
+        console.log(childData);
+    };
     return (
         <>
             <Child handleChildData={handleChildData} />
+            <Todo handleTodoData={handleTodoData} />
         </>
     );
 };
---
+++ src/CHILD-TO_PARENT/Todo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Todo = ({ handleTodoData }) => {
+    handleTodoData('Kopa');
+    return <div>Todo</div>;
+};
+
+export default Todo;
