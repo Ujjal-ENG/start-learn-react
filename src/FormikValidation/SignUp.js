74ed8e026ad456b89fcf5d9510f19c672aad9d40
user <ujjalroy7862@gmail.com>
2023-03-13 16:37:10 +0600

    error handing using yup

--- src/FormikValidation/SignUp.js
+++ src/FormikValidation/SignUp.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 const SignUp = () => {
     const formik = useFormik({
         initialValues: {
@@ -7,7 +8,12 @@ const SignUp = () => {
             email: '',
             password: ''
         },
+        validationSchema: yup.object({
+            name: yup.string().min(3, 'Name must have at least 3 char and maximum 30 char').max(30).required(),
 
+            email: yup.string().email().required(),
+            password: yup.string().min(6, 'Password Must have atleast 6 charracters')
+        }),
         onSubmit: (values, { resetForm }) => {
             console.log(values);
             resetForm({ values: '' });
@@ -21,9 +27,11 @@ const SignUp = () => {
             <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="name">Name: </label>
                 <input type="text" name="name" id="name" className="outline-orange-600 border-2 border-black rounded-md m-5" value={formik.values.name} onChange={formik.handleChange} />
+                {formik.errors.name && <span className="text-red-600">{formik.errors.name}</span>}
 
                 <label htmlFor="email">Email: </label>
                 <input type="email" name="email" id="email" className="outline-orange-600 border-2 border-black rounded-md m-5" value={formik.values.email} onChange={formik.handleChange} />
+                {formik.errors.email && <span className="text-red-600">{formik.errors.email}</span>}
 
                 <label htmlFor="password">Password: </label>
                 <input
@@ -34,6 +42,7 @@ const SignUp = () => {
                     value={formik.values.password}
                     onChange={formik.handleChange}
                 />
+                {formik.errors.password && <span className="text-red-600">{formik.errors.password}</span>}
 
                 <button type="submit" className="px-5 py-2 bg-violet-500 text-white text-xl font-bold rounded-md">
                     Submit Data
