53e92a3a36e8227733a7d98bb82af3f23e8da263
user <ujjalroy7862@gmail.com>
2023-03-15 02:18:03 +0600

    done the protected routing and protected my page very quickly

--- src/PROTECTED_ROUTING/routing/Index.js
+++ src/PROTECTED_ROUTING/routing/Index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import About from '../pages/About';
 import AddBlog from '../pages/AddBlog';
 import Home from '../pages/Home';
+import Protected from './Protected';
 
 const Index = () => {
+    const [isLoggedIn, setIsLoggegIn] = useState(false);
+
+    const handleLoggIn = () => {
+        setIsLoggegIn((ps) => !ps);
+    };
     return (
         <div>
+            <button className="px-4 py-1 bg-black text-white font-bold" type="submit" onClick={handleLoggIn}>
+                {isLoggedIn ? 'Logout' : 'LogIn'}
+            </button>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/add-blog" element={<AddBlog />} />
+
+                <Route
+                    path="/add-blog"
+                    element={
+                        <Protected isLoggedIn={isLoggedIn}>
+                            <AddBlog />
+                        </Protected>
+                    }
+                />
                 <Route path="/about" element={<About />} />
             </Routes>
         </div>
