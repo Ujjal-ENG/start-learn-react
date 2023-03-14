ea2848b5ac1e4be156c09dcdb741679e69883b6a
user <ujjalroy7862@gmail.com>
2023-03-15 01:45:51 +0600

    add dynamically data

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const User = () => {
-    return <div>User</div>;
+    const { userid } = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [name, serachName] = useState('');
+    const [id, serachId] = useState(0);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setSearchParams({ name: name, id: id });
+    };
+    return (
+        <div>
+            {searchParams.get('id')}
+            <br />
+            {searchParams.get('name')}
+            <form onSubmit={onSubmit}>
+                <input type="text" className="border-black border-2" value={name} onChange={(e) => serachName(e.target.value)} />
+                <input type="number" className="border-black border-2" value={id} onChange={(e) => serachId(e.target.value)} />
+                <button type="submit" className="px-2 bg-black text-white">
+                    Add
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default User;
