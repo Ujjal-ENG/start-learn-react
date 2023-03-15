1b11887003746972051e316205e466dd74287f18
user <ujjalroy7862@gmail.com>
2023-03-15 15:43:15 +0600

    done useCallback and useMemo

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ const App = () => {
     const onHandleIncrement = useCallback(() => {
         setCount((ps) => ps + 1);
     }, []);
+
+    const isEvenOrODD = (count) => {
+        return count % 2 === 0;
+    };
+
     return (
         <>
             <button onClick={() => setToggle((ps) => !ps)} className="px-4 py-1 bg-black text-white font-bold ">
                 {toggle ? 'ON' : 'OFF'}
             </button>
             <h1>Count: {count}</h1>
+            <h3>{isEvenOrODD(count) ? 'Even' : 'Odd'}</h3>
             <button onClick={() => setCount((ps) => ps + 1)} className="px-4 py-1 bg-black text-white font-bold ">
                 Increment
             </button>
