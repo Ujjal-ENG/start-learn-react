b30a6586b0b3e7208c8ddd4e05f313f5968a3b3b
user <ujjalroy7862@gmail.com>
2023-03-15 16:13:12 +0600

    start to learning react redux

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React, { useCallback, useState, useMemo } from 'react';
-import Message from './HOOKS_USE-MEMO/Message';
+import React from 'react';
 
 const App = () => {
-    console.log('App Rendering');
-    const [count, setCount] = useState(0);
-    const [toggle, setToggle] = useState(false);
-
-    const onHandleIncrement = useCallback(() => {
-        setCount((ps) => ps + 1);
-    }, []);
-
-    const isEvenOrODD = (count) => {
-        return count % 2 === 0;
-    };
-
-    return (
-        <>
-            <button onClick={() => setToggle((ps) => !ps)} className="px-4 py-1 bg-black text-white font-bold ">
-                {toggle ? 'ON' : 'OFF'}
-            </button>
-            <h1>Count: {count}</h1>
-            <h3>{isEvenOrODD(count) ? 'Even' : 'Odd'}</h3>
-            <button onClick={() => setCount((ps) => ps + 1)} className="px-4 py-1 bg-black text-white font-bold ">
-                Increment
-            </button>
-            <Message numberOfMessage={count} onHandleIncrement={onHandleIncrement} />
-        </>
-    );
+    return <></>;
 };
 
 export default App;
