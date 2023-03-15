14a5ed9a51b07aa521187e46cc2201fae4029022
user <ujjalroy7862@gmail.com>
2023-03-15 14:36:09 +0600

    done the useCallBack function use cases

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Message from './HOOKS_USE-MEMO/Message';
 
 const App = () => {
     console.log('App Rendering');
     const [count, setCount] = useState(0);
+    const [toggle, setToggle] = useState(false);
+
+    const onHandleIncrement = useCallback(() => {
+        setCount((ps) => ps + 1);
+    }, []);
     return (
         <>
+            <button onClick={() => setToggle((ps) => !ps)} className="px-4 py-1 bg-black text-white font-bold ">
+                {toggle ? 'ON' : 'OFF'}
+            </button>
             <h1>Count: {count}</h1>
-            <button onClick={() => setCount((ps) => ps + 1)} className="px-4 py-1 bg-black text-white font-bold active:px-3 active:py-1">
+            <button onClick={() => setCount((ps) => ps + 1)} className="px-4 py-1 bg-black text-white font-bold ">
                 Increment
             </button>
-            <Message numberOfMessage={count} />
+            <Message numberOfMessage={count} onHandleIncrement={onHandleIncrement} />
         </>
     );
 };
--- src/HOOKS_USE-MEMO/Message.js
+++ src/HOOKS_USE-MEMO/Message.js
@@ -1,8 +1,15 @@
 import React, { memo } from 'react';
 
-const Message = ({ numberOfMessage }) => {
+const Message = ({ numberOfMessage, onHandleIncrement }) => {
     console.log('Message Rendering');
-    return <div>Send {numberOfMessage} Message</div>;
+    return (
+        <div>
+            <p> Send {numberOfMessage} Message</p>
+            <button className="px-4 py-1 bg-black text-white font-bold " onClick={onHandleIncrement}>
+                Send Message
+            </button>
+        </div>
+    );
 };
 
 export default memo(Message);
